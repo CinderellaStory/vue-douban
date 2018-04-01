5063d77ce3b6dcb4751c602b8e71bc293c410141
谢念念 <1181148151@qq.com>
2018-04-02 01:14:57 +0800

    详情页跳转bug已解决2，开始制作电影详情页

--- src/components/MovieDetail.vue
+++ src/components/MovieDetail.vue
@@ -1,16 +1,7 @@
-// 正在热映
+// 电影详情页
 <template>
   <div class="MovieList">
-        <div>
-            <img v-lazy="subjects.images.medium" alt="" :key="index">
-            <div class="MovieText">
-                <div class="MovieTitle">{{subjects.title}}</div>
-                <div class="director">
-                    <p>导演：{{subjects.directors[0].name}}</p>
-                    <!-- <p>演员：<span v-for="(i,o) in items.casts" :key="o">{{items.casts[o].name}}/</span></p> -->
-                </div>                
-            </div>
-        </div>
+       <h1>我是电影详情页</h1>
     </div>
 </template>
 <script>
@@ -18,17 +9,8 @@
 export default {
   data(){
       return{
-          subjects:[]
          }
-      },
-      created(){
-        const _this = this;
-        this.axios({method:'get',url: '/api/movie/in_theaters'})
-        .then(response =>{
-            _this.data =response.data.subjects;
-            console.log(response.data.subjects);
-        })
-  }
+      }
 }
 </script>
 
