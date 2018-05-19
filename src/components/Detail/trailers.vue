72566b77841794330bf650a91277fe53f311295c
谢念念 <1181148151@qq.com>
2018-05-19 23:20:37 +0800

    提交视频详情页

--- src/components/Detail/trailers.vue
+++ src/components/Detail/trailers.vue
@@ -2,11 +2,12 @@
   <div class="trailers">
         <ul>
             <li class="video" v-for="(bg,b) in list" :key="b" v-bind:style="{'background-image':'url('+bg.medium+')'}">
-                <router-link :to="{name:'trailer',params:{id:bg.id}}">
+                <router-link :to="{path:'trailer',params:{id:bg.id}}">
                     <span><img src="https://img3.doubanio.com/f/talion/b6df390a5411896e81ad9be86a97121c17d4c805/pics/card/play-button.png" alt=""></span>
                 </router-link>
             </li>
         </ul>
+        <button @click="ClickMe">点击</button>
   </div>
 </template>
 
@@ -15,6 +16,11 @@ export default {
   name:"trailers",
   props:{
       list:Array
+  },
+  methods:{
+      ClickMe(){
+        console.log(this.list);
+      }
   }
 }
 </script>
